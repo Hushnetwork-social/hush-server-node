12ab3698953e12bdc01496e30572c936b6fab5bd
Paulo Aboim Pinto <aboimpinto@gmail.com>
2024-09-19 10:41:09 +0200

    Registering the FeedIndex strategies

--- Node/InternalModules/Feed/HushServerNode.InternalModule.Feed/FeedHostBuilder.cs
+++ Node/InternalModules/Feed/HushServerNode.InternalModule.Feed/FeedHostBuilder.cs
@@ -1,5 +1,7 @@
-﻿using HushServerNode.Interfaces;
+﻿using HushServerNode.Blockchain.IndexStrategies;
+using HushServerNode.Interfaces;
 using HushServerNode.InternalModule.Feed.Cache;
+using HushServerNode.InternalModule.Feed.IndexStrategies;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,6 +21,9 @@ public static class FeedHostBuilder
             services.AddSingleton<IDbContextConfigurator, CacheFeedDbContextConfigurator>();
             services.AddSingleton<CacheFeedDbContextConfigurator>();
 
+            // IndexStrategies
+            services.AddTransient<IIndexStrategy, FeedIndexStrategy>();
+            services.AddTransient<IIndexStrategy, FeedMessageIndexStrategy>();
         });
 
         return builder;
