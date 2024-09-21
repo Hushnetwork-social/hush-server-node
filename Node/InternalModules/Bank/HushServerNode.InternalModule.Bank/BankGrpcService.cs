3b1a2e787d509c83cde4d2f834993e06e4153508
Paulo Aboim Pinto <aboimpinto@gmail.com>
2024-09-21 13:21:48 +0200

    Create the bank gRPC that will handle and retrieve the funds of an address

---
+++ Node/InternalModules/Bank/HushServerNode.InternalModule.Bank/BankGrpcService.cs
@@ -0,0 +1,24 @@
+using Grpc.Core;
+using HushNetwork.proto;
+
+namespace HushServerNode.InternalModule.Bank;
+
+public class BankGrpcService : HushBank.HushBankBase
+{
+    private readonly IBankService _bankService;
+
+    public BankGrpcService(IBankService bankService)
+    {
+        this._bankService = bankService;
+    }
+
+    public override Task<GetAddressBalanceReply> GetAddressBalance(GetAddressBalanceRequest request, ServerCallContext context)
+    {
+        var balance = this._bankService.GetBalance(request.Address);
+
+        return Task.FromResult(new GetAddressBalanceReply
+        {
+            // Balance = balance
+        });
+    }
+}
