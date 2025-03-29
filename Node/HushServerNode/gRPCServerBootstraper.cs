995ae43cded7cdf3b1a51a66d12ea0f383446556
Paulo Aboim Pinto <aboimpinto@gmail.com>
2025-03-29 23:56:06 +0100

    Prepare the Hush to connect to internet address for gRPC instead of localhost

--- Node/HushServerNode/gRPCServerBootstraper.cs
+++ Node/HushServerNode/gRPCServerBootstraper.cs
@@ -30,7 +30,7 @@ public class gRPCServerBootstraper : IBootstrapper
     {
         var rcpServer = new Grpc.Core.Server
         {
-            Ports = { new ServerPort("localhost", 5000, ServerCredentials.Insecure) }
+            Ports = { new ServerPort("10.88.30.185", 5000, ServerCredentials.Insecure) }
         };
 
         foreach (var grpcDefinition in this._grpcDefinitions)
