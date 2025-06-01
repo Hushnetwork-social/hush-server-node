86d9cf8582eaa088d197dde40c0fceda65139ff7
Paulo Aboim Pinto <aboimpinto@gmail.com>
2025-06-01 23:32:10 +0200

    Showing a notification toaster and add message to the Notifications of the device.

---
+++ Shared/Olimpo.Android.Notifications/INotificationHandler.cs
@@ -0,0 +1,10 @@
+using System.Reactive.Subjects;
+
+namespace Olimpo.Android.Notifications;
+
+public interface INotificationHandler
+{
+    Subject<string> NotificationRequestSteam { get; }
+
+    void RegisterHandler();
+}
